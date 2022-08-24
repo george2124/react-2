61f16aa235dfb9e5aef9f76e0e570106a57c7749
Jorge Apaza <jorgeapaza2124@gmail.com>
2022-08-24 00:15:26 -0300

    agregando navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { red } from '@material-ui/core/colors';
+import NavBar from './components/NavBar';
 
 function App() {
+  let i = 0;
+  // i = i + 1;
+  // i++;
+  i == 0 ? console.log("increible la i es 0") : console.log("la i no es 0");;
+  console.log(i);
+  let estilos = {color: "#ff005"  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <h1>HOLA MUNDO</h1>
+      <p>algo mas para chamuyar</p>
+     
     </div>
   );
 }
